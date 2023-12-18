d47286e0bbf7bfe2f7da28431f83dabcd64486f8
jopiiyan <jovyanqiu@gmail.com>
2023-12-18 17:32:44 +0700

    final version

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -85,20 +85,20 @@ const Analytics = () => {
         </div>
       </div>
 
-      <div className="w-full bg-[black] text-white py-16 px-4">
-        <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+      <div className="w-full bg-[black] max-w-[1240px] text-white py-16 px-4">
+        <div className=" mx-auto grid md:grid-cols-2">
           <h1 className="font-extrabold text-transparent text-3xl md:text-4xl bg-clip-text bg-gradient-to-r from-[#cfcfcf] to-purple-700">
             Enter Your Redemption Code
           </h1>
           <div className="flex justify-center max-w-[500px] w-[90%] mt-6 md:mt-0">
             <input
-              className="flex-1 rounded-[0] text-2xl outline-none text-start font-bold p-3 z-50 text-black"
+              className="flex-1 rounded-[0] max-w-[300px lg:w-[100%] ] w-[80%] sm:w-full text-xl lg:text-2xl outline-none text-start font-bold p-3 z-50 text-black  "
               value={value}
               onChange={(e) => setValue(e.target.value)}
               type="text"
               placeholder="Redemption Code"
             />
-            <button className="font-bold bg-[#7655fa] w-full px-6 lg:max-w-[30%] hover:bg-[#6c4fe3] active:bg-[#553eb1] active:text-black ">
+            <button className="font-bold bg-[#7655fa] w-full max-w-[90px] px-4 hover:bg-[#6c4fe3] active:bg-[#553eb1] active:text-black ">
               Enter
             </button>
           </div>
